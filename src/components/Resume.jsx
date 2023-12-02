83b0edc3e6371b3e1ac73b6a84c961d58f17a990
supreeth <supreethratam@gmail.com>
2023-12-03 02:01:36 +0530

    added placements resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,11 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import Next from "./Next";
 import SocialIcons from "./SocialIcons";
 function Resume() {
-  const isMobile = window.innerWidth < 768;  
+  const isMobile = window.innerWidth < 768;
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <div className="md:px-10 mx-0 px-4 lg:mx-48 mt-28 lg:px-24 flex gap-6 h-full flex-col z-10 pb-14 text-article ">
@@ -17,20 +17,26 @@ function Resume() {
         <p>
           Here is my resume, you can{" "}
           <a
-            href="https://drive.google.com/file/d/19riaMFOQpYnD8tjZ2BbRZU4KDZKL0KGz/preview"
+            href="https://drive.google.com/file/d/1UiwdEsYgGFiKJXmh0Yi7o5pHBcUa-1aN/preview"
             className="uppercase font-bold text-sm hover:underline inline"
-          >View</a> {" "} or{" "}
+          >
+            View
+          </a>{" "}
+          or{" "}
           <a
-            href="https://drive.google.com/file/d/19riaMFOQpYnD8tjZ2BbRZU4KDZKL0KGz/"
+            href="https://drive.google.com/file/d/1UiwdEsYgGFiKJXmh0Yi7o5pHBcUa-1aN/preview"
             className="uppercase font-bold text-sm hover:underline inline"
-          >Download</a> {" "} it.
+          >
+            Download
+          </a>{" "}
+          it.
         </p>
-        
+
         <iframe
-          src="https://drive.google.com/file/d/19riaMFOQpYnD8tjZ2BbRZU4KDZKL0KGz/preview"
+          src="https://drive.google.com/file/d/1UiwdEsYgGFiKJXmh0Yi7o5pHBcUa-1aN/preview"
           className="w-full lg:w-3/4"
-        //   height="1000"
-          height={isMobile?"450":"1000"}
+          //   height="1000"
+          height={isMobile ? "450" : "1000"}
           allow="autoplay"
         ></iframe>
         <Next text={"Contact Me Now"} path={"/contact"} />
--- src/data/about.jsx
+++ src/data/about.jsx
@@ -1,63 +1,90 @@
-import {FaGraduationCap} from 'react-icons/fa'
-import {BiTestTube} from 'react-icons/bi'
-import { TbBooks } from 'react-icons/tb'
-
+import { FaGraduationCap } from "react-icons/fa";
+import { BiTestTube } from "react-icons/bi";
+import { TbBooks } from "react-icons/tb";
 
 export const aboutStats = [
-    {
-        title: 'Engineering',
-        place: 'Mechanical Engineering IIT Bombay, 2024',
-        description: 'When it comes to business, first impressions matter, and good website design is the key to capitalizing on them. An excellent site is not judged solely on its looks, but on its functionality and usability as well. My experience as a programmer allows me to come up with intelligent solutions to technical challenges, while at the same time designing sleek and visually appealing websites. Aside from having extensive knowledge of recognized technical standards, I am conversant with modern building practices.',
-        icon: FaGraduationCap
-    },
-    {
-        title: 'Intermediate',
-        place: 'Narayana Junior Collage, 2020',
-        description: "In spite of not being an atypical product manager, I have strong backgrounds in research, product design, and product coordination that can help the product grow from 0 to 1. As a first-rate analytical thinker, I am able to maintain the product's vision from start to finish, both technically and product-wise.",
-        icon: BiTestTube
-    },
-    {
-        title: 'Secondary',
-        place: 'Narayana eCO School, 2018',
-        description: 'Although I am not the typical designer, I do possess excellent visual abilities, and this makes me an excellent presenter, allowing me to successfully communicate design ideas to stakeholders and design teams. With my strong conceptualization ability, I am able to visualize and create beautiful works. Lastly, you can find me mostly tweaking stylesheets and creating fluid user experiences.',
-        icon: TbBooks
-    },
-]
+  {
+    title: "Engineering",
+    place: "Mechanical Engineering IIT Bombay, 2024",
+    description:
+      "When it comes to business, first impressions matter, and good website design is the key to capitalizing on them. An excellent site is not judged solely on its looks, but on its functionality and usability as well. My experience as a programmer allows me to come up with intelligent solutions to technical challenges, while at the same time designing sleek and visually appealing websites. Aside from having extensive knowledge of recognized technical standards, I am conversant with modern building practices.",
+    icon: FaGraduationCap,
+  },
+  {
+    title: "Intermediate",
+    place: "Narayana Junior Collage, 2020",
+    description:
+      "In spite of not being an atypical product manager, I have strong backgrounds in research, product design, and product coordination that can help the product grow from 0 to 1. As a first-rate analytical thinker, I am able to maintain the product's vision from start to finish, both technically and product-wise.",
+    icon: BiTestTube,
+  },
+  {
+    title: "Secondary",
+    place: "Narayana eCO School, 2018",
+    description:
+      "Although I am not the typical designer, I do possess excellent visual abilities, and this makes me an excellent presenter, allowing me to successfully communicate design ideas to stakeholders and design teams. With my strong conceptualization ability, I am able to visualize and create beautiful works. Lastly, you can find me mostly tweaking stylesheets and creating fluid user experiences.",
+    icon: TbBooks,
+  },
+];
 
 export const experience = [
-    {
-        title: 'Azzet app',
-        place: 'D&H Fintech Solution Pvt Ltd',
-        description:'Created and implemented an innovative hybrid mobile platform for automotive applications using JavaScript and React Native, achieving significant code sharing between iOS and Android and reducing development time. Utilized Git for version control of source code and ensured smooth maintenance of builds. Employed reduxjs for efficient state management and utilized react-native-reanimated for intricate SVGs, charts, and Candlestick graphs.'
-    },
-    {
-        title: 'Web Manager',
-        place: 'Insight, IIT Bombay',
-        description: <>Led and managed the Web Team responsible for maintaining Insight, IIT Bombay's official website and multiple microsites such as mba-guide and gs-meter. Mentored and trained web convenors, guiding them through the development of various microsites. Successfully migrated the Wordpress website from an EC Cloud instance on AWS to IIT Bombay's gymkhana servers, ensuring seamless continuity of online services.</>
-    },
-    {
-        title: 'Web Secretary',
-        place: 'Mechanical Engineering Association, IIT Bombay',
-        description: <>Managed and maintained the Department website in collaboration with the Faculty Coordinator and council members. Strategized and executed website updates and facilitated data collection from council members. Designed and implemented an invoice generator, project portal, and council team page, streamlining departmental operations and improving online accessibility for students and faculty.</>
-    },
-]
+  {
+    title: "Fernglasz app",
+    place: "Edupherz",
+    description:
+      "Leveraged React Native to convert designs into code and integrated REST APIs, ensuring a responsive user experience \n Collaborated with developers to co-design and enhance APIs, promoting smooth integration from frontend to backend \n Implemented AWS S3 Bucket integration for efficient storage management, enhancing system reliability and performance \n Employed Flask and OpenAI APIs to create an efficient chatbot, strategically devised prompts for optimized response time",
+  },
+  {
+    title: "Azzet app",
+    place: "D&H Fintech Solution Pvt Ltd",
+    description:
+      "Created and implemented an innovative hybrid mobile platform for automotive applications using JavaScript and React Native, achieving significant code sharing between iOS and Android and reducing development time. Utilized Git for version control of source code and ensured smooth maintenance of builds. Employed reduxjs for efficient state management and utilized react-native-reanimated for intricate SVGs, charts, and Candlestick graphs.",
+  },
+  {
+    title: "Web Manager",
+    place: "Insight, IIT Bombay",
+    description: (
+      <>
+        Led and managed the Web Team responsible for maintaining Insight, IIT
+        Bombay's official website and multiple microsites such as mba-guide and
+        gs-meter. Mentored and trained web convenors, guiding them through the
+        development of various microsites. Successfully migrated the Wordpress
+        website from an EC Cloud instance on AWS to IIT Bombay's gymkhana
+        servers, ensuring seamless continuity of online services.
+      </>
+    ),
+  },
+  {
+    title: "Web Secretary",
+    place: "Mechanical Engineering Association, IIT Bombay",
+    description: (
+      <>
+        Managed and maintained the Department website in collaboration with the
+        Faculty Coordinator and council members. Strategized and executed
+        website updates and facilitated data collection from council members.
+        Designed and implemented an invoice generator, project portal, and
+        council team page, streamlining departmental operations and improving
+        online accessibility for students and faculty.
+      </>
+    ),
+  },
+];
 
 export const skills = [
-    'Python',
-    'C++',
-    'JavaScript',
-    'HTML',
-    'CSS',
-    'React Js',
-    'Node Js',
-    'Redux',
-    'FastAPI',
-    'MySQL',
-    'MongoDB',
-    'Firebase',
-    'PowerBI',
-    'SciKit Learn',
-    'Numpy',
-    'Pandas',
-    'Excel'
-]
+  "Python",
+  "C++",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "React Js",
+  "Node Js",
+  "Redux",
+  "FastAPI",
+  "MySQL",
+  "MongoDB",
+  "Firebase",
+  "PowerBI",
+  "SciKit Learn",
+  "Numpy",
+  "Pandas",
+  "Excel",
+];
